ee4aa4c0d88c944b1fafabd64775c6945367afa5
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-12-21 23:38:19 +0000

    Task 1 fix 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,13 +5,15 @@ Fabric script that generates a .tgz archive from the contents of the web_static 
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
-    """Generate the .tgz archive"""
+    """Generates .tgz archive from the /web_static's content
+    """
     now = datetime.now().strftime('%Y%m%d%H%M%S')
     filePath = 'versions/web_static_{}.tgz'.format(now)
 
-    local('mkdir -p versions')
-    created = local("tar -cvzf {} web_static".format(filePath))
+    local('mkdir -p versions/')
+    created = local('tar -cvzf {} web_static/'.format(filePath))
 
     if created.succeeded:
         return filePath
